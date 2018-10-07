13946f39edf1e82f5a4902c553fb0c8d26537525
Kenny Kim <kenny@dynawebworks.com>
2018-10-07 10:35:32 -0700

    Add bootstrap

---
+++ src/components/scheduleDisplay.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ScheduleDisplay extends Component {
+	state = {}
+
+	render() {
+		return (  
+			<h1> Schedule Name </h1>
+		)
+	}
+
+}
