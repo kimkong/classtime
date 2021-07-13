5650ba130bd7b41984840732d57813a399301258
Kenny Kim <kenny@dynawebworks.com>
2021-07-13 10:49:21 -0700

    updating wip to new block schedule for 2021

--- schedule_data.js
+++ schedule_data.js
@@ -19,94 +19,74 @@ function test_pertime(min_offset) {
 
 data = [
 {
-	name: "Regular Day MS",
-	dow: [2,3,4,5,6,0],
-	type: "MS",
+	name: "Regular Block ODD",
+	dow: [2,4,6,0],
+	type: "HS",
 	periods: [
-		{ name: 'Period 0 (Optional)',	start_time: '6:44',  end_time: '7:54' },
-		{ name: 'Passing to Period 1',	start_time: '7:54',  end_time: '8:00' },
-		{ name: 'Period 1',							start_time: '8:00',  end_time: '8:58' },
-		{ name: 'Passing to Period 2',	start_time: '8:58',  end_time: '9:04' },
-		{ name: 'Period 2',							start_time: '9:04',  end_time: '10:10' },
+		{ name: 'Period 0 (Optional)',	start_time: '7:20',  end_time: '8:21' },
+		{ name: 'Passing to Period 1',	start_time: '8:21',  end_time: '8:30' },
+    { name: 'Period 1',             start_time: '8:30',  end_time: '9:50' },
+		{ name: 'Period 1 Support',	  	start_time: '9:50',  end_time: '10:10' },
 		{ name: 'Snack',								start_time: '10:10', end_time: '10:19' },
 		{ name: 'Passing to Period 3',	start_time: '10:19', end_time: '10:25' },
-		{ name: 'Period 3',							start_time: '10:25', end_time: '11:23' },
-		{ name: 'Lunch (7-8)',					start_time: '11:23', end_time: '11:54' },
-		{ name: 'Passing (7-8)',				start_time: '11:54', end_time: '12:00' },
-		{ name: 'Period 4 (7-8)',				start_time: '12:00', end_time: '12:58' },
-		{ name: 'Passing to Period 5',	start_time: '12:58', end_time: '13:04' },
-		{ name: 'Period 5',							start_time: '13:04', end_time: '14:02' },
-		{ name: 'Passing to Period 6',	start_time: '14:02', end_time: '14:08' },
-		{ name: 'Period 6',							start_time: '14:08', end_time: '15:06' }
+    { name: 'Period 3',             start_time: '10:25', end_time: '11:45' },
+		{ name: 'Period 3 Support', 		start_time: '11:45', end_time: '12:05' },
+		{ name: 'Lunch',        				start_time: '12:05', end_time: '12:45' },
+		{ name: 'Passing to Period 5',	start_time: '12:45', end_time: '12:51' },
+    { name: 'Period 5',             start_time: '12:51', end_time: '14:11' },
+		{ name: 'Period 5 Support',			start_time: '14:11', end_time: '14:31' },
+		{ name: 'Passing to P.R.I.D.E.',start_time: '14:31', end_time: '14:37' },
+		{ name: 'P.R.I.D.E.',						start_time: '14:37', end_time: '15:36' }
 	]
 },
 {
-	name: "Regular Day HS",
-	dow: [2,3,4,5,6,0],
+	name: "Regular Block EVEN",
+	dow: [3,5,0],
 	type: "HS",
 	periods: [
-		{ name: 'Period 0 (Optional)',	start_time: '6:44',  end_time: '7:54' },
-		{ name: 'Passing to Period 1',	start_time: '7:54',  end_time: '8:00' },
-		{ name: 'Period 1',							start_time: '8:00',  end_time: '8:58' },
-		{ name: 'Passing to Period 2',	start_time: '8:58',  end_time: '9:04' },
-		{ name: 'Period 2',							start_time: '9:04',  end_time: '10:10' },
-		{ name: 'Snack',								start_time: '10:10', end_time: '10:19' },
-		{ name: 'Passing to Period 3',	start_time: '10:19', end_time: '10:25' },
-		{ name: 'Period 3',							start_time: '10:25', end_time: '11:23' },
-		{ name: 'Passing (9-12)',				start_time: '11:23', end_time: '11:29' },
-		{ name: 'Period 4 (9-12)',			start_time: '11:29', end_time: '12:27' },
-		{ name: 'Lunch (9-12)',					start_time: '12:27', end_time: '12:58' },
-		{ name: 'Passing to Period 5',	start_time: '12:58', end_time: '13:04' },
-		{ name: 'Period 5',							start_time: '13:04',  end_time: '14:02' },
-		{ name: 'Passing to Period 6',	start_time: '14:02',  end_time: '14:08' },
-		{ name: 'Period 6',							start_time: '14:08',  end_time: '15:06' }
+    { name: 'Period 0 (Optional)',  start_time: '7:20',  end_time: '8:21' },
+    { name: 'Passing to Period 2',  start_time: '8:21',  end_time: '8:30' },
+    { name: 'Period 2',             start_time: '8:30',  end_time: '9:50' },
+    { name: 'Period 2 Support',     start_time: '9:50',  end_time: '10:10' },
+    { name: 'Snack',                start_time: '10:10', end_time: '10:19' },
+    { name: 'Passing to Period 4',  start_time: '10:19', end_time: '10:25' },
+    { name: 'Period 4',             start_time: '10:25', end_time: '11:45' },
+    { name: 'Period 4 Support',     start_time: '11:45', end_time: '12:05' },
+    { name: 'Lunch',                start_time: '12:05', end_time: '12:45' },
+    { name: 'Passing to Period 6',  start_time: '12:45', end_time: '12:51' },
+    { name: 'Period 6',             start_time: '12:51', end_time: '14:11' },
+    { name: 'Period 6 Support',     start_time: '14:11', end_time: '14:31' },
+    { name: 'Passing to P.R.I.D.E.',start_time: '14:31', end_time: '14:37' },
+    { name: 'P.R.I.D.E.',           start_time: '14:37', end_time: '15:36' }
 	]
 },
 
+
 {
-	name: "Shortened Day MS",
-	dow: [1],
-	type: "MS",
-	periods: [
-		{ name: 'Passing to Period 1', 	start_time: '9:24',  end_time: '9:30' },
-		{ name: 'Period 1', 						start_time: '9:30',  end_time: '10:14' },
-		{ name: 'Passing to Period 2', 	start_time: '10:14', end_time: '10:20' },
-		{ name: 'Period 2', 						start_time: '10:20', end_time: '11:07' },
-		{ name: 'Snack', 								start_time: '11:07', end_time: '11:16' },
-		{ name: 'Passing to Period 3', 	start_time: '11:16', end_time: '11:22' },
-		{ name: 'Period 3', 						start_time: '11:22', end_time: '12:06' },
-		{ name: 'Lunch (7-8)',					start_time: '12:06', end_time: '12:37' },
-		{ name: 'Passing (7-8)',				start_time: '12:37', end_time: '12:43' },
-		{ name: 'Period 4 (7-8)',				start_time: '12:43', end_time: '13:27' },
-		{ name: 'Passing to Period 5',	start_time: '13:27',  end_time: '13:33' },
-		{ name: 'Period 5',							start_time: '13:33',  end_time: '14:17' },
-		{ name: 'Passing to Period 6',	start_time: '14:17',  end_time: '14:23' },
-		{ name: 'Period 6',							start_time: '14:23',  end_time: '15:06' }
-	]
-},
-{
-	name: "Shortened Day HS",
+	name: "Block Monday",
 	dow: [1],
 	type: "HS",
 	periods: [
-		{ name: 'Passing to Period 1', 	start_time: '9:24',  end_time: '9:30' },
-		{ name: 'Period 1', 						start_time: '9:30',  end_time: '10:14' },
-		{ name: 'Passing to Period 2', 	start_time: '10:14', end_time: '10:20' },
-		{ name: 'Period 2', 						start_time: '10:20', end_time: '11:07' },
-		{ name: 'Snack', 								start_time: '11:07', end_time: '11:16' },
-		{ name: 'Passing to Period 3', 	start_time: '11:16', end_time: '11:22' },
-		{ name: 'Period 3', 						start_time: '11:22', end_time: '12:06' },
-		{ name: 'Passing (9-12)', 			start_time: '12:06', end_time: '12:12' },
-		{ name: 'Period 4 (9-12)', 			start_time: '12:12', end_time: '12:56' },
-		{ name: 'Lunch (9-12)', 				start_time: '12:56', end_time: '13:27' },
-		{ name: 'Passing to Period 5',	start_time: '13:27',  end_time: '13:33' },
-		{ name: 'Period 5',							start_time: '13:33',  end_time: '14:17' },
-		{ name: 'Passing to Period 6',	start_time: '14:17',  end_time: '14:23' },
-		{ name: 'Period 6',							start_time: '14:23',  end_time: '15:06' }
+		{ name: 'Passing to Period 1', 	start_time: '8:30',  end_time: '10:00' },
+		{ name: 'Period 1', 						start_time: '10:00', end_time: '10:44' },
+		{ name: 'Passing to Period 2', 	start_time: '10:44', end_time: '10:50' },
+		{ name: 'Period 2', 						start_time: '10:50', end_time: '11:36' },
+		{ name: 'Snack', 								start_time: '11:36', end_time: '11:45' },
+		{ name: 'Passing to Period 3', 	start_time: '11:45', end_time: '11:51' },
+		{ name: 'Period 3', 						start_time: '11:51', end_time: '12:35' },
+		{ name: 'Passing to Period 4',  start_time: '12:35', end_time: '12:41' },
+		{ name: 'Period 4',       			start_time: '12:41', end_time: '13:25' },
+		{ name: 'Lunch',         				start_time: '13:25', end_time: '13:56' },
+		{ name: 'Passing to Period 5',	start_time: '13:56', end_time: '14:02' },
+		{ name: 'Period 5',							start_time: '14:02', end_time: '14:46' },
+		{ name: 'Passing to Period 6',	start_time: '14:46', end_time: '14:52' },
+		{ name: 'Period 6',							start_time: '14:52', end_time: '15:36' }
 	]
 },
+
+
 {
-	name: "Finals",
+	name: "Finals ??",
 	dow: [],
 	type: "",
 	periods: [
@@ -123,69 +103,45 @@ data = [
 	dow: [],
 	type: "",
 	periods: [
-		{ name: 'Period 0 (Optional)', 		start_time: '6:44',  end_time: '7:54' },
-		{ name: 'Passing to Period 1', 		start_time: '7:54',  end_time: '8:00' },
-		{ name: 'Period 1', 							start_time: '8:00',  end_time: '8:41' },
-		{ name: 'Passing to Period 2', 		start_time: '8:41',  end_time: '8:47' },
-		{ name: 'Period 2', 							start_time: '8:47',  end_time: '9:29' },
-		{ name: 'Passing to Period 3', 		start_time: '9:29',  end_time: '9:35' },
-		{ name: 'Period 3', 							start_time: '9:35',  end_time: '10:16' },
-		{ name: 'Snack', 									start_time: '10:16', end_time: '10:24' },
-		{ name: 'Passing to Period 4', 		start_time: '10:24', end_time: '10:30' },
-		{ name: 'Period 4', 							start_time: '10:31', end_time: '11:12' },
-		{ name: 'Passing to Period 5', 		start_time: '11:12', end_time: '11:18' },
-		{ name: 'Period 5', 							start_time: '11:18', end_time: '11:59' },
-		{ name: 'Passing to Period 6', 		start_time: '11:59', end_time: '12:05' },
-		{ name: 'Period 6', 							start_time: '12:05', end_time: '12:46' }	
+		{ name: 'Period 0 (Optional)', 		start_time: '7:20',  end_time: '8:21' },
+		{ name: 'Passing to Period 1', 		start_time: '8:21',  end_time: '8:30' },
+		{ name: 'Period 1', 							start_time: '8:30',  end_time: '9:11' },
+		{ name: 'Passing to Period 2', 		start_time: '9:11',  end_time: '9:17' },
+		{ name: 'Period 2', 							start_time: '9:17',  end_time: '9:59' },
+		{ name: 'Passing to Period 3', 		start_time: '9:59',  end_time: '10:05' },
+		{ name: 'Period 3', 							start_time: '10:05', end_time: '10:46' },
+		{ name: 'Snack', 									start_time: '10:46', end_time: '10:55' },
+		{ name: 'Passing to Period 4', 		start_time: '10:55', end_time: '11:01' },
+		{ name: 'Period 4', 							start_time: '11:01', end_time: '11:42' },
+		{ name: 'Passing to Period 5', 		start_time: '11:42', end_time: '11:48' },
+		{ name: 'Period 5', 							start_time: '11:48', end_time: '12:29' },
+		{ name: 'Passing to Period 6', 		start_time: '12:29', end_time: '12:35' },
+		{ name: 'Period 6', 							start_time: '12:35', end_time: '13:16' }	
 	]
 },
 {
-	// WTF?
-	name: "RallyA",
-	dow: [],
-	type: "",
-	periods: [
-		{ name: 'Passing to Period 1', 		start_time: '7:54',  end_time: '8:00' },
-		{ name: 'Period 1', 							start_time: '8:00',  end_time: '8:46' },
-		{ name: 'Passing to Period 2', 		start_time: '8:46',  end_time: '8:52' },
-		{ name: 'Period 2', 							start_time: '8:52',  end_time: '9:42' },
-		{ name: 'Snack', 									start_time: '9:42',  end_time: '9:52' },
-		{ name: 'Passing to Period 3', 		start_time: '9:52',  end_time: '9:58' },
-		{ name: 'Period 3', 							start_time: '9:58',  end_time: '10:44' },
-		{ name: 'Passing to Period 4', 		start_time: '10:44', end_time: '10:50' },
-
-		{ name: '4A Rally', 							start_time: '10:50', end_time: '11:43' },
-		{ name: 'Period 4', 							start_time: '11:43', end_time: '12:42' },
-		{ name: 'Lunch', 									start_time: '12:42', end_time: '1:22' },
-
-		{ name: 'Passing to Period 5', 		start_time: '13:22', end_time: '13:28' },
-		{ name: 'Period 5', 							start_time: '13:28', end_time: '14:14' },
-		{ name: 'Passing to Period 6', 		start_time: '14:14', end_time: '14:20' },
-		{ name: 'Period 6', 							start_time: '14:20', end_time: '15:06' }	
-	]
-},
-{
-	// WTF?
-	name: "RallyB",
-	dow: [],
-	type: "",
-	periods: [
-		{ name: 'Passing to Period 1', 		start_time: '7:54',  end_time: '8:00' },
-		{ name: 'Period 1', 							start_time: '8:00',  end_time: '8:46' },
-		{ name: 'Passing to Period 2', 		start_time: '8:46',  end_time: '8:52' },
-		{ name: 'Period 2', 							start_time: '8:52',  end_time: '9:42' },
-		{ name: 'Snack', 									start_time: '9:42',  end_time: '9:52' },
-		{ name: 'Passing to Period 3', 		start_time: '9:52',  end_time: '9:58' },
-		{ name: 'Period 3', 							start_time: '9:58',  end_time: '10:44' },
-		{ name: 'Passing to Period 4', 		start_time: '10:44', end_time: '10:50' },
-		{ name: '4A Rally', 							start_time: '10:50', end_time: '11:43' },
-		{ name: 'Period 4', 							start_time: '11:43', end_time: '12:42' },
-		{ name: 'Lunch', 									start_time: '12:42', end_time: '1:22' },
-		{ name: 'Passing to Period 5', 		start_time: '13:22', end_time: '13:28' },
-		{ name: 'Period 5', 							start_time: '13:28', end_time: '14:14' },
-		{ name: 'Passing to Period 6', 		start_time: '14:14', end_time: '14:20' },
-		{ name: 'Period 6', 							start_time: '14:20', end_time: '15:06' }	
-	]
+  name: "Rally",
+  dow: [],
+  type: "",
+  periods: [
+    { name: 'Period 0 (Optional)',    start_time: '7:20',  end_time: '8:21' },
+    { name: 'Passing to Period 1',    start_time: '8:21',  end_time: '8:30' },
+    { name: 'Period 1',               start_time: '8:30',  end_time: '9:15' },
+    { name: 'Passing to Period 2',    start_time: '9:15',  end_time: '9:21' },
+    { name: 'Period 2',               start_time: '9:21',  end_time: '10:11' },
+    { name: 'Snack',                  start_time: '10:11', end_time: '10:21' },
+    { name: 'Passing to Period 3',    start_time: '10:21', end_time: '10:27' },
+    { name: 'Period 3',               start_time: '10:27', end_time: '11:12' },
+    { name: 'Passing to Rally 4A',    start_time: '11:12', end_time: '11:18' },
+    { name: 'Rally 4A',               start_time: '11:18', end_time: '12:11' },
+    { name: 'Passing to Rally 4B',    start_time: '12:11', end_time: '12:21' },
+    { name: 'Rally 4B',               start_time: '12:21', end_time: '13:14' },
+    { name: 'Lunch',                  start_time: '13:14', end_time: '13:54' },
+    { name: 'Passing to Period 5',    start_time: '13:54', end_time: '14:00' },
+    { name: 'Period 5',               start_time: '14:00', end_time: '14:45' },
+    { name: 'Passing to Period 6',    start_time: '14:45', end_time: '14:51' },
+    { name: 'Period 6',               start_time: '14:51', end_time: '15:36' }  
+  ]
 },
 {
 	name: "Block",
@@ -204,7 +160,7 @@ data = [
 	]
 },
 {
-	name: "Test",
+	name: "Test ??",
 	dow: [],
 	type: "",
 	periods: [
